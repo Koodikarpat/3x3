63b79a9b91ba11b069d4a2e65f8bc7d25d3155d2
Thommenius <tommi99@windowslive.com>
2017-06-05 15:09:46 +0300

    nappuloita

---
+++ Assets/Scripts/ButtonSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSelection : MonoBehaviour 
+{
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	public void click(int button)
+	{
+		
+	}
+}
