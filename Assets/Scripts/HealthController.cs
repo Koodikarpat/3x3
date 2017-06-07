807dc89093ec1c3df55fb01736d54412e3ac5895
Olli Tomberg <ollitomberg@Olli-MacBook-Pro.local>
2017-06-07 15:03:23 +0300

    Elämäpisteet

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour {
+
+	public int startingHealth = 20;
+	public int currentHealth;
+	Text HealthCounter; 
+
+
+	public void TakeDamage (int amount)
+	{
+			currentHealth -= amount;
+
+		HealthCounter.text = ""+currentHealth;
+
+		if (currentHealth <= 0) {
+			Debug.Log("Game Over");
+
+		}
+
+		}
+
+	// Use this for initialization
+	void Start () {
+		HealthCounter = GetComponent < Text > ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			TakeDamage (1);
+		}
+
+
+
+				
+	}
+}
