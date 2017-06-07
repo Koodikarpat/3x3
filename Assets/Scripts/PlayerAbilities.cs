e6d3e0af8fc862bac9a733d77df858c52a2548fa
Thommenius <tommi99@windowslive.com>
2017-06-07 15:09:10 +0300

    laatoille värit ja nimet

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -5,34 +5,42 @@ using UnityEngine.UI;
 
 public class PlayerAbilities : MonoBehaviour 
 {
-
-	//Array for all the mapbuttons, the movement of the players will be restricted to only these buttons.
-	public GameObject[] buttonArray = new GameObject[9];
+	
+	public GameObject[] buttonArray = new GameObject[9];//Array for all the mapbuttons, the movement of the players will be restricted to only these buttons.
 	public GameObject buttonGroup;
+	public Color[] buttonColors;
 	private Vector2[] buttonPosition = new Vector2[9];
 	int currentButton;
 
+
+
 	void Start () 
 	{
+
 		//Keeping track of the location of the player gameobject; "what button is the player on at the moment"
 		currentButton = 4;
 
-		for (int i = 0; i < 9; i++)
+		for (int i = 0; i < 9; i++) 
+		{
 			buttonPosition [i] = buttonArray [i].transform.position;
+			//buttonArray [i] = buttonColors [i];
+		}
 		
 	}
 
 	void Update () 
 	{
+		/*
 		//Getting the mousedown click on buttons to get player moving on the selected location.
 		ButtonSelection buttonSelection = buttonGroup.GetComponent<ButtonSelection> ();
 		if (Input.GetMouseButtonUp (0))
 			Debug.Log ("release");
-
+		*/
 	}
 
 	public void MoveButton(int button)
 	{
+		//The movement itself.
 		if (isLegalMove (currentButton, button)) 
 		{
 			transform.position = buttonPosition [button];
