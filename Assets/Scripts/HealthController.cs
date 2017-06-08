66eaa9149371aa670b9e6e4f1976f260486f9565
Olli Tomberg <ollitomberg@Olli-MacBook-Pro.local>
2017-06-08 13:10:38 +0300

    elämäpisteiden laskuri, damage ja heal

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -7,37 +7,50 @@ public class HealthController : MonoBehaviour {
 
 	public int startingHealth = 20;
 	public int currentHealth;
+	public GameObject GameOverText;
 	Text HealthCounter; 
 
+	private bool GameOver;
 
 	public void TakeDamage (int amount)
 	{
-			currentHealth -= amount;
+		currentHealth -= amount;
 
-		HealthCounter.text = ""+currentHealth;
+		if (currentHealth <= 0){
+			currentHealth = 0;
+			GameOver = true;
+			GameOverText.GetComponent < Text > ().text = "Game Over!";
+	}
 
-		if (currentHealth <= 0) {
-			Debug.Log("Game Over");
+		HealthCounter.text = "" + currentHealth;
+	}
 
-		}
 
-		}
+	public void Heal (int amount)
+	{
+
+		currentHealth += amount;
+
+	}
+
 
 	// Use this for initialization
 	void Start () {
 		HealthCounter = GetComponent < Text > ();
-		
+		GameOver = false;
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetMouseButtonDown(0)) {
-			TakeDamage (1);
-		}
+			TakeDamage (1); //amount of damage
+	}
 
+		if (Input.GetMouseButtonDown (1)) {
+			Heal (1); //amount of heal
 
+	}
 
-				
 	}
 }
