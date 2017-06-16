8a4f795699eae3bae69a8b0b66c6df3d07104fdf
Mirka Ylikoski <mirka.ylikoski@student.oulu.fi>
2017-06-16 10:22:16 +0300

    health images also change

--- Assets/Scripts/FindToken.cs
+++ Assets/Scripts/FindToken.cs
@@ -7,10 +7,20 @@ public class FindToken : MonoBehaviour {
 
 	GameObject sceneController;
 
+	//player1
 	Image player1Token; //player1: toimii
+	public GameObject player1;
 
-	Image player2RandomToken;//test
-	public GameObject player2TokenTest; //test
+	//player2
+	Image player2RandomToken;//test -	WORKS!	
+	public GameObject player2TokenTest; //test WORKS!	
+
+	//health-images
+	Image player1HealthImage;
+	public GameObject player1Health;
+	Image player2HealthImage;
+	public GameObject player2Health;
+
 
 	void Awake (){
 		sceneController = GameObject.Find ("SceneController"); //että löytää
@@ -19,11 +29,20 @@ public class FindToken : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		SceneControllerScript sceneControllerScript = sceneController.GetComponent<SceneControllerScript> (); //toimii - kerro että tämä on tämä
-		player1Token = GetComponent<Image> (); //toimii - että tietää kuva komponentin
+		//player 1
+		player1Token = player1.GetComponent<Image> (); //toimii - että tietää kuva komponentin
 		player1Token.sprite = sceneControllerScript.tokenSprite; //toimii - currentSprite(TokenControl)= tokenSprite(SceneChanges - SceneControllerScript)= player1Token(FindToken)
-
+	
+		//player 2 -atm random
 		player2RandomToken = player2TokenTest.GetComponent<Image> ();
 		player2RandomToken.sprite = sceneControllerScript.randomTokenSprite;
+
+		//health-images
+		player1HealthImage = player1Health.GetComponent<Image> ();
+		player1HealthImage.sprite = sceneControllerScript.tokenSprite;
+
+		player2HealthImage = player2Health.GetComponent<Image> ();
+		player2HealthImage.sprite = sceneControllerScript.randomTokenSprite;
 	}
 	
 	// Update is called once per frame
