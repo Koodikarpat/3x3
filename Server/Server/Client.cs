b88be99fec8dad4697ab97ac18a9742ee77688d1
axuttaja <axuttaja@users.noreply.github.com>
2017-06-26 09:59:29 +0300

    serveri pois asseteista ja kerroksen ylemmäs

---
+++ Server/Server/Client.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Reflection;
+using System.Net.Sockets;
+using System.ComponentModel;
+
+namespace Networking
+{
+	public class Client
+	{
+		private const uint PROTO_VERSION = 0; // Every time you make chance that breaks the interface, you should increment this
+		public const int PORT = 2500;
+
+		private readonly string serverName;
+
+		private readonly string serverUsername;
+		private readonly string serverAuthToken;
+
+		private TcpClient server;
+		private NetworkStream serverStream;
+		private BinaryWriter serverWriter;
+		private BinaryReader serverReader;
+
+		private Parser parser;
+
+		public Client(string name, string username = "player", string token = "password")
+		{
+			serverName = name;
+			serverUsername = username;
+			serverAuthToken = token;
+		}
+
+		public int Connect()
+		{
+			server = new TcpClient();
+
+			parser = new Parser();
+
+			try {
+				server.Connect(serverName, PORT);
+			} catch {
+				Log("Connecting to: " + serverName + ":" + PORT + " failed.");
+				return 1;
+			}
+
+			serverStream = server.GetStream();
+			serverWriter = new BinaryWriter(server.GetStream());
+			serverReader = new BinaryReader (server.GetStream());
+
+			// create a new worker that will handle the connection
+			BackgroundWorker waiter = new BackgroundWorker();
+			waiter.DoWork += (object sender, DoWorkEventArgs e) => WaitForUpdate();
+			waiter.RunWorkerAsync();
+
+			Authenticate();
+
+			return 0;
+		}
+
+		public void Disconnect()
+		{
+			server.Close();
+		}
+
+		private void Authenticate() {
+			Message.AuthenticationRequest
+		}
+
+		private void WaitForUpdate()
+		{
+			while (server.Connected) {
+				parser.RecvObject (serverReader, OnUpdate); // blocks until a message is received
+				// the callback should be run in the main thread, currently that doesn't happen 
+			}
+		}
+		
+		private void OnUpdate(object msg)
+		{
+		}
+
+		private void Request(object req)
+		{
+		}
+
+		private void SendObject(object msg)
+		{
+			parser.SendObject(serverWriter, msg);
+		}
+
+		private void Log(string msg)
+		{
+			Console.WriteLine(msg);
+		}
+	}
+}
---
+++ Server/Server/Parser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Networking
+{
+	public class Parser
+	{
+		private JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings() { 
+			TypeNameHandling = TypeNameHandling.All
+		};
+
+		public Parser()
+		{
+		}
+
+		public bool SendObject(BinaryWriter writer, object message) // returns connected status
+		{
+			string json = JsonConvert.SerializeObject(message, jsonSerializerSettings);
+			try {
+				writer.Write(json);
+			} catch {
+				return false;
+			}
+			return true;
+		}
+
+		public int RecvObject(BinaryReader reader, Action<object> callback) { // returns 0 if succesfull
+			string json;
+			try {
+				json = reader.ReadString();
+
+				Object message = JsonConvert.DeserializeObject(json, jsonSerializerSettings);
+
+				callback(message);
+			} catch {
+				Console.WriteLine("fail");
+				return 1;
+			}
+			return 0;
+		}
+	}	
+}
