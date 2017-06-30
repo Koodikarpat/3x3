e9e2af4dda639c53f9252a5ecaa06bbac2ff76a7
Antti111 <antti.oikku@yahoo.com>
2017-06-30 13:25:14 +0300

    kissan animaatio ja päävalikon kynttilät

---
+++ Assets/Scripts/VisualEffects/Elf.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elf : MonoBehaviour {
+
+	void Update() {
+		//transform.Translate(Vector3.right * Time.deltaTime );
+	}
+}
---
+++ Assets/Scripts/VisualEffects/Flicker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flicker : MonoBehaviour {
+
+	public Image myImage;
+
+	void Start(){
+		myImage = GetComponent<Image> ();
+	}
+
+	void Update () {
+		float x = Time.time;
+		float darkness = Mathf.PerlinNoise(x,0);
+		darkness = darkness / 2 +0.3f;
+		myImage.color = new Color(darkness, darkness, darkness, 1f);
+		//myImage.CrossFadeAlpha (1.0f, 1.0f, true);//CrossFadeAlpha(Alpha, Time, Ignore time scale)
+	}
+}
+  
