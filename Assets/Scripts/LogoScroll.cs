e53811c40e7605b7fff5cad355d3bd18eef08410
Mikcsu <miika.puominen@gmail.com>
2017-07-14 15:30:10 +0300

    kissa

---
+++ Assets/Scripts/LogoScroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogoScroll : MonoBehaviour
+{
+    public float movementSpeed = 1.2f;
+    
+    void Update()
+    {
+        if (transform.position.y < 300)
+        {
+            transform.Translate(Vector2.up * movementSpeed);
+        }
+    }
+}
