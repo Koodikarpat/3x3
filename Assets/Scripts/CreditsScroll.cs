bda3def4e2c2a06f2d254210d2e807f458694d98
Mikcsu <miika.puominen@gmail.com>
2017-07-14 15:28:03 +0300

    Lopputekstit ja päävalikko

---
+++ Assets/Scripts/CreditsScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsScroll : MonoBehaviour
+{
+    public float movementSpeed = 3;
+    public TextAsset text;
+
+    void Start()
+    {
+        if(text != null)
+            GetComponent<Text>().text = text.text;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Translate(Vector2.up * movementSpeed);
+	}
+}
