9b9afab136c79b64aed85a4137cf0972fcf848ea
ibanezi <bekim.abazi95@gmail.com>
2017-07-14 10:46:13 +0300

    Added inventory parsing

--- Server/Server/SqlFunctions.cs
+++ Server/Server/SqlFunctions.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 
 /*
-These functions are made by using MySqlConnector version 6.8.9 in Visual Studio
+These functions are made with use of  MySqlConnector version 6.9.8.0 in Visual Studio
 To use thses, you must have MySql server and database already set
-Remember to add MySql.Data to references in Visual Studio so it will work
+Remember to add MySql.Data extension to references in Visual Studio so it will work
 There are still bits of code that could be tidyed up
 */
+
+//TODO: Methods to server for calling these methods
 
 namespace sqlFunctions{
 
@@ -18,12 +20,13 @@ public class mySqlFunctions{
 
     String server = "127.0.0.1";    //THESE ARE TEST VARIABLES TO CONNECT INTO MYSQL DATABASE
     String port = "3306";           //FOR NOW EVERYTHING WORKS IN TEST DATABASE
-    String database = "players";    
+    String database = "pelaajat";    
     String mysqlpassword = "Testi";
     Int32 rank = 100;
     String deviceId = "Testi4";
     String username = "Peetu";
-    String inventory = "1:2:3";
+    String inventory;
+    String[] playerInventory;
 
         public void newUser(){
 
@@ -43,7 +46,7 @@ public class mySqlFunctions{
 			object exists = command.ExecuteScalar(); //Returns null if not in database
 
 			if(exists == null){
-				command.CommandText = "insert into users (id,deviceId,username,rank,created,last_online) values(NULL,?deviceId,?username, NULL, NULL, NULL)";
+				command.CommandText = "insert into users (id,deviceId,username,rank,inventory,last_online,created) values(NULL,?deviceId,?username, 0, '1:2:3', NULL, NULL)";
 				//command.Parameters.AddWithValue("?deviceId", deviceId);
 				command.Parameters.AddWithValue("?username", username);
 				command.ExecuteNonQuery();
@@ -82,11 +85,11 @@ public class mySqlFunctions{
                 {
                     rank = reader.GetInt32("rank");
                     username = reader.GetString("username");
-                    //inventory = reader.GetString("inventory");
-                    //getPlayerInventory();
-                    Console.WriteLine(rank);
+                    inventory = reader.GetString("inventory");
+                    playerInventory = getPlayerInventory();
+                    //Console.WriteLine(rank);
                     Console.WriteLine(username);
-                    Console.WriteLine(inventory.ToString());
+                    //Console.WriteLine(playerInventory.ToString());
                 }
                 else { Console.WriteLine("No such thing"); }
         }
@@ -166,7 +169,7 @@ public class mySqlFunctions{
         MySqlConnection conn = new MySqlConnection(myConnectionString);
         MySqlCommand command = conn.CreateCommand();
 
-        createInventoryString();
+        inventory = createInventoryString();
         command.CommandText = "Update users SET inventory=?inventory WHERE deviceId=?deviceId";
         command.Parameters.AddWithValue("?inventory", inventory);
         command.Parameters.AddWithValue("?deviceId", deviceId);
@@ -175,18 +178,36 @@ public class mySqlFunctions{
         	conn.Open();
         	command.ExecuteNonQuery();
         }catch (MySqlException ex){
-        	Console.WriteLine(ex.Message)
+                Console.WriteLine(ex.Message);
         }
         conn.Close();
 	}
 
-	public void getPlayerInventory(){
+	public string[] getPlayerInventory(){
 		char[] separatingChars = { ':' };
-		string[] playerInventory = inventory.Split(separatingChars, StringSplitOptions.RemoveEmptyEntries);
+            string[] playerInventory = inventory.Split(separatingChars, StringSplitOptions.RemoveEmptyEntries);
+
+            //List<string> test = new List<string>(playerInventory);
+            //test.Add("9");
+            //Console.WriteLine(test);
+
+            //These are for testing
+            Console.WriteLine("Entrys in database string");
+            foreach (string s in playerInventory)
+            {
+                Console.WriteLine(s.ToString());
+            }
+
+            return playerInventory;
 	}
 
-	public void createInventoryString(){
-		string inventory = string.Join(":", playerInventory);
+	public string createInventoryString(){
+            //string inventory = "4:5:6";
+            string inventory = string.Join(":", playerInventory);
+            //These prints are for testing
+            Console.WriteLine("Transformed string");
+            Console.WriteLine(inventory);
+            return inventory;
 	
 	}
 }}
